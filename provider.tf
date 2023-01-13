6e6fe816203cc3f46fffd9866c04c2b1b3c73c1c
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-01-13 16:37:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,3 @@
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
